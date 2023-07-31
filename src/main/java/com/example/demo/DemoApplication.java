037394522118b3f421dc7067f399ed7cc91a5f27
shankarpc07 <140762683+shankarpc07@users.noreply.github.com>
2023-07-31 17:15:46 -0500

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.sql.*;
 
 @SpringBootApplication
 @RestController
@@ -12,13 +11,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Try{
-		Connection connection = DriverManager.getConnection("jdbc:sqlserver://pricedb07.database.windows.net:1433;database=Priceit;user=dbadmin@pricedb07;password=Admin123;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
-	
-		} catch(Exception e){
-			system.out.println("connection established hopefully");
-		}
 	}
+	
 	@RequestMapping("/")
 	String sayHello() {
 		return "Hello Shankar!";
